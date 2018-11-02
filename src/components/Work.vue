72abd8a287ca06873baed55eb2b8cca74193dc23
borimoon <boriana@freemail.hu>
2018-11-02 13:04:12 +0100

    pattern, favicon

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -1,20 +1,22 @@
 <template>
   <section class="work">
-    <div class="work-title">
-      <h1 class="main-heading">Case studies</h1>
-      <h2 class="subheading">(Freelance projects)</h2>
-    </div>
+    <div class="work-content">
+      <div class="work-title">
+        <h1 class="main-heading">Case studies</h1>
+        <h2 class="subheading">(Freelance projects)</h2>
+      </div>
 
-    <ul class="cases">
-      <router-link :to="caseStudy.href" tag="li" v-for="caseStudy in caseStudies" :key="caseStudy.title">
-        <a>
-          <img class="image" :src="caseStudy.src" :alt="caseStudy.title">
-          <p><strong>{{ caseStudy.title }}</strong></p>
-          <p>{{ caseStudy.role }}</p>
-          <small>{{ caseStudy.time }}</small>
-        </a>
-      </router-link>
-    </ul>
+      <ul class="cases">
+        <router-link :to="caseStudy.href" tag="li" v-for="caseStudy in caseStudies" :key="caseStudy.title">
+          <a>
+            <img class="image" :src="caseStudy.src" :alt="caseStudy.title">
+            <p><strong>{{ caseStudy.title }}</strong></p>
+            <p>{{ caseStudy.role }}</p>
+            <small>{{ caseStudy.time }}</small>
+          </a>
+        </router-link>
+      </ul>
+    </div>
   </section>
 </template>
 
